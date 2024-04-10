dc64d8a29690e113b16aec8014edad6f9d3c847d
LeoHenriot <162290277+LeoHenriot@users.noreply.github.com>
2024-04-10 08:37:48 +0200

    Ajouté main et affichage

---
+++ affichage/AfficheDonjon.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void AfficheDonjon(const vector<vector<bool>>& matrice_adjacence, int taille) {
+    
+    // Première ligne
+    cout<<"┌";
+    for (int i=0; i<taille; ++i) {
+        if (i != 1) cout<<"─";
+        else cout<<" ";
+        if (i != taille-1) {
+            cout<<"┬";
+        }
+    }
+    cout<<"┐"<<endl;
+
+    //Lignes intermédiaires
+    for (int i=0; i<taille; ++i) {
+        cout<<"│";
+        for (int j=0; j<taille; ++j) {
+            cout<<" ";
+            if (j != taille-1) {
+                if (matrice_adjacence[i * taille + j][i * taille + j + 1]) // Vérifie le noeud suivant
+                    cout<<" "; // Pas de mur
+                else
+                    cout<<"│"; // Mur
+            }
+        }
+        cout<<"│"<<endl;
+        
+        if (i != taille-1) {
+            cout<<"├";
+            for (int j=0; j<taille; ++j) {
+                if (matrice_adjacence[i * taille + j][(i + 1) * taille + j]) // Vérifie le noeud en dessous
+                    cout<<" "; // Pas de mur
+                else
+                    cout<<"─"; // Mur
+                if (j != taille-1) cout<<"┼";
+            }
+            cout<<"┤"<<endl;
+        }
+    }
+
+    // Dernière ligne
+    cout<<"└";
+    for (int i=0; i<taille; ++i) {
+        cout<<"─";
+        if (i != taille-1) {
+            cout<<"┴";
+        }
+    }
+    cout<<"┘"<<endl;
+}
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include "affichage/AfficheDonjon.cpp"
+
+using namespace std;
+
+int main() {
+   
+    // On demande à l'utilisateur la taille du labyrinthe
+    int taille=0;
+    while (taille <= 0 || taille > 50) {
+        cout<<"Entrez la taille du labyrinthe : ";
+        cin>>taille;
+        if (taille <= 0 || taille > 50) {
+            cout<<"[!] Veuillez rentrer une valeur entre 1 et 50."<<endl;
+        }
+    }
+
+    // Création d'une matrice d'adjacence de n*n taille (pour lier pouvoir lier tout les sommets)
+    vector<vector<bool>> matrice_adjacence(taille*taille, vector<bool>(taille*taille, false));
+
+    AfficheDonjon(matrice_adjacence, taille);
+
+
+// ---------------------------------------------------------------------------
+
+/*
+    // Création d'une matrice d'adjacence de 3x3
+    vector<vector<bool>> matrice_adjacence(9, vector<bool>(9, false));
+    
+    AfficheDonjon(matrice_adjacence, 3);
+    // Modifier quelques connexions pour illustrer l'exemple
+    matrice_adjacence[0][1] = true; // Lien entre (0,0) et (0,1)
+    matrice_adjacence[1][0] = true;
+
+    matrice_adjacence[3][6] = true; // Lien entre (0,1) et (0,2)
+    matrice_adjacence[6][3] = true;
+
+    matrice_adjacence[1][4] = true; // Lien entre (1,0) et (1,1)
+    matrice_adjacence[4][1] = true;
+    
+    AfficheDonjon(matrice_adjacence, 3);
+*/
+    return 0;
+}
