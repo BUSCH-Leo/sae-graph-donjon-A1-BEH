d703bd91c548b1c603d02822e082e2b9e55dd457
LeoHenriot <162290277+LeoHenriot@users.noreply.github.com>
2024-05-03 11:15:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <iostream>
 #include <vector>
+#include <time.h>
 #include <windows.h>
 #include "affichage/AfficheDonjon.cpp"
+#include "affichage/AfficheDijkstra.cpp"
 #include "generation/generateur.cpp"
-#include <time.h>
+#include "parcours/Dijkstra.cpp"
+#include "chargement/chargement.cpp"
 
 using namespace std;
 
@@ -11,7 +15,12 @@ int main() {
 
     srand(time(NULL));
 
-    int entreX=1;
+    FauxChargement(50);
+    system("Clear");
+
+    cout<<"\n██╗      █████╗ ██████╗ ██╗   ██╗███████╗███████╗███████╗███████╗███████╗\n██║     ██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝\n██║     ███████║██████╔╝ ╚████╔╝ █████╗  █████╗  ███████╗███████╗█████╗  \n██║     ██╔══██║██╔══██╗  ╚██╔╝  ██╔══╝  ██╔══╝  ╚════██║╚════██║██╔══╝  \n███████╗██║  ██║██████╔╝   ██║   ██║     ███████╗███████║███████║███████╗\n╚══════╝╚═╝  ╚═╝╚═════╝    ╚═╝   ╚═╝     ╚══════╝╚══════╝╚══════╝╚══════╝\n"<<endl;
+
+    int entreX=0;
     int entreY=0;
 
     SetConsoleOutputCP(CP_UTF8); // pour éviter d'avoir mauvais charactère
@@ -29,7 +38,18 @@ int main() {
     // Création d'une matrice d'adjacence de n*n taille (pour lier pouvoir lier tout les sommets)   
     vector<vector<bool>> matrice_adjacence(taille * taille, vector<bool>(taille * taille, false));
     int sortie = LabyrintheParfait(matrice_adjacence, taille, entreX, entreY);
+
     AfficheDonjon(matrice_adjacence, taille);
+
+    cout<<endl;
+
+    sortie = LabyrintheParfait(matrice_adjacence, taille, entreX, entreY);
+
+    AfficheDonjon(matrice_adjacence, taille);
+
+    vector<pair<int, int>> cheminParcouru = Dijkstra(matrice_adjacence, taille, 0, 0, taille-1, taille-1);
+
+    AfficheDijkstra(matrice_adjacence, taille, cheminParcouru);
 
     return 0;
 }
