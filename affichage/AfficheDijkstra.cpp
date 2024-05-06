7b49e05f0276a9bfecab30a446f20dd3ae4763f1
LeoHenriot <162290277+LeoHenriot@users.noreply.github.com>
2024-05-06 12:28:59 +0200

    Update AfficheDijkstra.cpp

--- affichage/AfficheDijkstra.cpp
+++ affichage/AfficheDijkstra.cpp
@@ -6,14 +6,15 @@ using namespace std;
 void AfficheDijkstra(const vector<vector<bool>>& matrice_adjacence, int taille, const vector<pair<int, int>>& cheminDijkstra) {
     
     // Première ligne
+    cout<<"  ";
     for (int i=0; i<taille*2+1; ++i) {
-        cout<<"█";
+        cout<<"██";
     }
     cout<<endl;
 
     //Lignes intermédiaires
     for (int i=0; i<taille; ++i) {
-        cout<<"█";
+        cout<<"  ██";
         for (int j=0; j<taille; ++j) {
             bool estCheminDijkstra = false;
             for (const auto& coord : cheminDijkstra) {
@@ -23,27 +24,57 @@ void AfficheDijkstra(const vector<vector<bool>>& matrice_adjacence, int taille, 
                 }
             }
             if (estCheminDijkstra) {
-                cout << "\033[33m∎\033[0m"; // Affiche "O" pour indiquer le chemin de Dijkstra
+                cout << "\033[34m▓▓\033[0m"; // Affiche "O" pour indiquer le chemin de Dijkstra
             } else {
-                cout << "·"; // Affiche l'espace si ce n'est pas le chemin de Dijkstra
+                cout << "░░"; // Affiche l'espace si ce n'est pas le chemin de Dijkstra
             }
             
             if (j != taille-1) {
-                if (matrice_adjacence[i * taille + j][i * taille + j + 1]) // Vérifie le noeud suivant
-                    cout<<" "; // Pas de mur
-                else
-                    cout<<"█"; // Mur
+                if (matrice_adjacence[i * taille + j][i * taille + j + 1]) {// Vérifie le noeud suivant
+                    estCheminDijkstra = false;
+                    for (const auto& coord : cheminDijkstra) {
+                        if (coord.first == j && coord.second == i) {
+                            estCheminDijkstra = true;
+                            break;
+                        }
+                    }
+                    bool estCheminDijkstra2 = false;
+                    for (const auto& coord : cheminDijkstra) {
+                        if (coord.first == (j+1) && coord.second == (i)) {
+                            estCheminDijkstra2 = true;
+                            break;
+                        }
+                    }
+                    if (estCheminDijkstra2 && estCheminDijkstra) cout<<"\033[34m▒▒\033[0m";
+                    else cout<<"  "; // Pas de mur
+                } else
+                    cout<<"██"; // Mur
             }
         }
-        cout<<"█"<<endl;
+        cout<<"██"<<endl;
         
         if (i != taille-1) {
-            cout<<"█";
+            cout<<"  ██";
             for (int j=0; j<taille; ++j) {
-                if (matrice_adjacence[i * taille + j][(i + 1) * taille + j]) // Vérifie le noeud en dessous
-                    cout<<" "; // Pas de mur
-                else
-                    cout<<"█"; // Mur
+                if (matrice_adjacence[i * taille + j][(i + 1) * taille + j]) {// Vérifie le noeud en dessous
+                    bool estCheminDijkstra = false;
+                    for (const auto& coord : cheminDijkstra) {
+                        if (coord.first == j && coord.second == i) {
+                            estCheminDijkstra = true;
+                            break;
+                        }
+                    }
+                    bool estCheminDijkstra2 = false;
+                    for (const auto& coord : cheminDijkstra) {
+                        if (coord.first == (j) && coord.second == (i+1)) {
+                            estCheminDijkstra2 = true;
+                            break;
+                        }
+                    }
+                    if (estCheminDijkstra2 && estCheminDijkstra) cout<<"\033[34m▒▒\033[0m";
+                    else cout<<"  "; // Pas de mur
+                } else
+                    cout<<"██"; // Mur
                 if (j != taille-1) {
                     // Vérifier si les 4 nœuds adjacents sont reliés
                     if (j < taille - 1 && i < taille - 1 &&
@@ -51,19 +82,20 @@ void AfficheDijkstra(const vector<vector<bool>>& matrice_adjacence, int taille, 
                         matrice_adjacence[i * taille + j][(i + 1) * taille + j] && // Nœud en dessous
                         matrice_adjacence[i * taille + j + 1][(i + 1) * taille + j +1] && // Nœud en diagonale en bas à droite
                         matrice_adjacence[(i + 1) * taille + j][(i + 1) * taille + j + 1]) { // Nœud en diagonale en bas à gauche
-                        cout << " "; // Pas de mur
+                        cout << "  "; // Pas de mur
                     } else {
-                        cout << "█"; // Mur
+                        cout << "██"; // Mur
                     }
                 }
             }
-            cout<<"█"<<endl;
+            cout<<"██"<<endl;
         }
     }
 
     // Dernière ligne
+    cout<<"  ";
     for (int i=0; i<taille*2+1; ++i) {
-        cout<<"█";
+        cout<<"██";
     }
     cout<<endl;
 }
