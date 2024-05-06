07a692893ea0acbd6b8e375b14dab9d86ca2b32e
LeoHenriot <162290277+LeoHenriot@users.noreply.github.com>
2024-05-06 12:29:18 +0200

    Update AfficheDonjon.cpp

--- affichage/AfficheDonjon.cpp
+++ affichage/AfficheDonjon.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void AfficheDonjon(const vector<vector<bool>>& matrice_adjacence, int taille) {
     
     // Première ligne
+    cout<<"  ";
     for (int i=0; i<taille*2+1; ++i) {
         cout<<"██";
     }
@@ -13,9 +14,9 @@ void AfficheDonjon(const vector<vector<bool>>& matrice_adjacence, int taille) {
 
     //Lignes intermédiaires
     for (int i=0; i<taille; ++i) {
-        cout<<"██";
+        cout<<"  ██";
         for (int j=0; j<taille; ++j) {
-            cout<<"··";
+            cout<<"  ";
             if (j != taille-1) {
                 if (matrice_adjacence[i * taille + j][i * taille + j + 1]) // Vérifie le noeud suivant
                     cout<<"  "; // Pas de mur
@@ -27,7 +28,7 @@ void AfficheDonjon(const vector<vector<bool>>& matrice_adjacence, int taille) {
         cout<<"██"<<endl;
         
         if (i != taille-1) {
-            cout<<"██";
+            cout<<"  ██";
             for (int j=0; j<taille; ++j) {
                 if (matrice_adjacence[i * taille + j][(i + 1) * taille + j]) // Vérifie le noeud en dessous
                     cout<<"  "; // Pas de mur
@@ -51,10 +52,9 @@ void AfficheDonjon(const vector<vector<bool>>& matrice_adjacence, int taille) {
     }
 
     // Dernière ligne
+    cout<<"  ";
     for (int i=0; i<taille*2+1; ++i) {
         cout<<"██";
     }
     cout<<endl;
-}
-
 }
