cc526df56d319d41a4929c32f9cccd0ae1cd48cb
LeoHenriot <162290277+LeoHenriot@users.noreply.github.com>
2024-05-13 18:40:46 +0200

    Modification du choix des seeds
    Nous pouvons maintenant créer des seeds à base de caractère ascii, donnant la possibilité de mettre des seeds amusantes en toutes lettres. Bien mieux que de taper des chiffres

--- main.cpp
+++ main.cpp
@@ -20,8 +20,15 @@ int main() {
     /*
     FauxChargement(50);
     */
+    system("Clear");
 
-    unsigned int seed;
+    cout<<"\n     █████████   ███████████   █████ █████\n    ███░░░░░███ ░░███░░░░░███ ░░███ ░░███ \n   ░███    ░███  ░███    ░███  ░░███ ███  \n   ░███████████  ░██████████    ░░█████   \n   ░███░░░░░███  ░███░░░░░███    ███░███  \n   ░███    ░███  ░███    ░███   ███ ░░███ \n   █████   █████ █████   █████ █████ █████\n  ░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░ ░░░░░ \n"<<endl;
+
+    int entreX=0;
+    int entreY=0;
+
+    string seed;
+    int seed_convert;
     char choix_seed;
     cout << "Voulez-vous entrer une seed manuellement ? (o/n) : ";
     cin >> choix_seed;
@@ -35,26 +42,26 @@ int main() {
         seed = rd();
         cout << "Seed aléatoire générée : " << seed << endl;
     }
-    srand(seed);
-    cout << "la seed est :" << seed << endl;
-    Sleep(5000);
+    for (char c : seed) {
+        int asciiValue = static_cast<int>(c);
+        seed_convert = seed_convert * 10 + asciiValue;
+    }
 
-    system("Clear");
+    cout << "seed_convert final : " << seed_convert << endl;
 
-    cout<<"\n     █████████   ███████████   █████ █████\n    ███░░░░░███ ░░███░░░░░███ ░░███ ░░███ \n   ░███    ░███  ░███    ░███  ░░███ ███  \n   ░███████████  ░██████████    ░░█████   \n   ░███░░░░░███  ░███░░░░░███    ███░███  \n   ░███    ░███  ░███    ░███   ███ ░░███ \n   █████   █████ █████   █████ █████ █████\n  ░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░ ░░░░░ \n"<<endl;
-
-    int entreX=0;
-    int entreY=0;
+    srand(seed_convert);
+    cout << "La seed choisi est : " << seed << endl;
 
     // On demande à l'utilisateur la taille du labyrinthe
     int taille = 0;
     while (taille <= 0 || taille > 160) {
-        cout << "Entrez la taille du labyrinthe : ";
+        cout << "\nEntrez la taille du labyrinthe : ";
         cin >> taille;
         if (taille <= 0 || taille > 160) {
             cout << "[!] Veuillez rentrer une valeur entre 1 et 160." << endl;
         }
     }
+    cout<<endl;
 
     int sortix = rand() % taille;
     int sorty = rand() % taille;
@@ -68,14 +75,17 @@ int main() {
     AfficheDonjon(matrice_adjacenceParfaite, taille, sortix, sorty);
     vector<pair<int, int>> cheminParfait = Dijkstra(matrice_adjacenceParfaite, taille, 0, 0, sortix, sorty);
     AfficheDijkstra(matrice_adjacenceParfaite, taille, cheminParfait, sortix, sorty);
+    cout<<"Labyrinthe 'Plus que parfait' :"<<endl;
     int sortiePlusqueparfaite = LabyrinthePlusQueParfait(matrice_adjacencePlusqueparfaite, taille, entreX, entreY);
     AfficheDonjon(matrice_adjacencePlusqueparfaite, taille, sortix, sorty);
+    cout<<"\n"<<endl;
     vector<pair<int, int>> cheminPlusqueparfait = Dijkstra(matrice_adjacencePlusqueparfaite, taille, 0, 0, sortix, sorty);
     AfficheDijkstra(matrice_adjacencePlusqueparfaite, taille, cheminPlusqueparfait, sortix, sorty);
     int sortieImparfaite = LabyrintheImparfait(matrice_adjacenceImparfaite, taille, entreX, entreY, 5);
     AfficheDonjon(matrice_adjacenceImparfaite, taille, sortix, sorty);
     vector<pair<int, int>> cheminImparfait = Dijkstra(matrice_adjacenceImparfaite, taille, 0, 0, sortix, sorty);
     AfficheDijkstra(matrice_adjacenceImparfaite, taille, cheminImparfait, sortix, sorty);
+    
     return 0;
 }
 
