202c4625325a4f70da1296c0b65330bc082a2fb5
LeoHenriot <162290277+LeoHenriot@users.noreply.github.com>
2024-05-16 20:22:28 +0200

    Changement du shop
    Ajout du multiplicateur d'argent, achetable

--- jeu/shop.cpp
+++ jeu/shop.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int Shop(int joueur_coins, bool pass_paye) {
+int Shop(int joueur_coins, bool pass_paye, int nb_bonus) {
     int choix = 0;
 
     termkit::clear();
@@ -10,20 +10,22 @@ int Shop(int joueur_coins, bool pass_paye) {
     cout << "\n     Bienvenue dans le magasin ! Voici votre argent : " << joueur_coins << " 💰" << endl;
 
     cout << "\n     Voici les articles possible :" << endl;
-    cout << "     - Pass PlusCourtChemin (20 💰)   -> Choix 1" << endl;
-    cout << "     - InstaKill sur 1 ennemi (5 💰)  -> Choix 2" << endl;
-    cout << "     - Potion de vie +5 ♥ (10 💰)     -> Choix 3" << endl;
-    cout << "     - Téléportation aléatoire (5 💰) -> Choix 4" << endl;
-    cout << "     - Ressortir                      -> Choix 0" << endl;
+    cout << "     - Pass PlusCourtChemin (20 💰)     -> Choix 1" << endl;
+    cout << "     - InstaKill sur 1 ennemi (5 💰)    -> Choix 2" << endl;
+    cout << "     - Potion de vie +5 ♥ (10 💰)       -> Choix 3" << endl;
+    cout << "     - Téléportation aléatoire (5 💰)   -> Choix 4" << endl;
+    cout << "     - Double l'argent des sacs (" << 10*nb_bonus << " 💰) -> Choix 5" << endl;
+    cout << "     - Ressortir                        -> Choix 0" << endl;
 
     cout << "     Votre choix : ";
     cin >> choix;
 
-    while (choix < 0 || choix > 4 
+    while (choix < 0 || choix > 5
     || (choix == 1 && joueur_coins - 20 < 0)
     || (choix == 2 && joueur_coins - 5 < 0)
     || (choix == 3 && joueur_coins - 10 < 0)
     || (choix == 4 && joueur_coins - 5 < 0)
+    || (choix == 5 && ((joueur_coins - (10*nb_bonus)) < 0))
     || (pass_paye == true && choix == 1)
     ) {
         if (pass_paye == true && choix == 1) {
