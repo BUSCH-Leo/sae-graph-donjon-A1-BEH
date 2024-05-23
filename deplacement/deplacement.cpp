173a9ab30f48ca5b03f02cb680ed180e610dd5b9
léo <156224120+BUSCH-Leo@users.noreply.github.com>
2024-05-23 09:50:20 +0200

    visibilité deplacement
    ajouté la fonction pour le fog of war dans deplacement

--- deplacement/deplacement.cpp
+++ deplacement/deplacement.cpp
@@ -2,55 +2,80 @@
 #include <vector>
 #include <algorithm>
 #include <random>
-#include "../listes/listes.cpp"
 
 using namespace std;
 
-visibilite(vector<vector<bool>>& matrice_adjacence, int taille, int entreX, int entreY) {
-	int cellule = (entreX + entreY * taille);
-	vector<bool>vue(taille * taille, false);
-	vue[cellule]=true
-    //sud
-    while (cellule - taille >= 0 && matrice_adjacence[cellule][cellule-taille]) {
+vector<bool> visibilite(const vector<vector<bool>>& matrice_adjacence, int taille, int entreX, int entreY) {
+    int cellule = (entreX + entreY * taille);
+    vector<bool> vue(taille * taille, false);
+    vue[cellule] = true;
+
+    // sud
+    while (cellule - taille >= 0 && matrice_adjacence[cellule][cellule - taille]) {
         cellule = cellule - taille;
-        vue[cellule] = true
+        vue[cellule] = true;
     }
-    //nord
+
+    // nord
+    cellule = (entreX + entreY * taille);  // Reset cellule
     while (cellule + taille < taille * taille && matrice_adjacence[cellule][cellule + taille]) {
         cellule = cellule + taille;
-        vue[cellule] = true
+        vue[cellule] = true;
     }
-    //ouest
+
+    // ouest
+    cellule = (entreX + entreY * taille);  // Reset cellule
     while (cellule % taille != 0 && matrice_adjacence[cellule][cellule - 1]) {
         cellule = cellule - 1;
-        vue[cellule] = true
+        vue[cellule] = true;
     }
-    //est
+
+    // est
+    cellule = (entreX + entreY * taille);  // Reset cellule
     while ((cellule + 1) % taille != 0 && matrice_adjacence[cellule][cellule + 1]) {
         cellule = cellule + 1;
-        vue[cellule] = true
+        vue[cellule] = true;
     }
 
-    //nord-est
-    while (cellule - taille >= 0 && (cellule + 1) % taille != 0 && matrice_adjacence[cellule][cellule - taille] && matrice_adjacence[cellule][cellule + 1] && matrice_adjacence[(cellule - taille) + 1][cellule - taille] && matrice_adjacence[(cellule - taille) + 1][cellule + 1]) {
-        cellule = (cellule - taille) - 1;
-        vue[cellule] = true
+    // nord-est
+    cellule = (entreX + entreY * taille);  // Reset cellule
+    while (cellule - taille >= 0 && (cellule + 1) % taille != 0 &&
+        matrice_adjacence[cellule][cellule - taille] &&
+        matrice_adjacence[cellule][cellule + 1] &&
+        matrice_adjacence[cellule - taille][cellule - taille + 1]) {
+        cellule = (cellule - taille) + 1;
+        vue[cellule] = true;
     }
-    //sud-est
-    while (cellule + taille < taille * taille && (cellule + 1) % taille != 0 && matrice_adjacence[cellule][cellule + taille] && matrice_adjacence[cellule][cellule + 1] && matrice_adjacence[(cellule + taille) + 1][cellule + taille] && matrice_adjacence[(cellule + taille) + 1][cellule + 1]) {
+
+    // sud-est
+    cellule = (entreX + entreY * taille);  // Reset cellule
+    while (cellule + taille < taille * taille && (cellule + 1) % taille != 0 &&
+        matrice_adjacence[cellule][cellule + taille] &&
+        matrice_adjacence[cellule][cellule + 1] &&
+        matrice_adjacence[cellule + taille][cellule + taille + 1]) {
         cellule = (cellule + taille) + 1;
-        vue[cellule] = true
-    }
-    //sud-ouest
-    while (cellule + taille < taille * taille && cellule % taille != 0 && matrice_adjacence[cellule][cellule + taille] && matrice_adjacence[cellule][cellule - 1 && matrice_adjacence[(cellule + taille) - 1][cellule + taille] && matrice_adjacence[(cellule + taille) - 1][cellule - 1]) {
-        cellule = (cellule + taille) - 1;
-        vue[cellule] = true
+        vue[cellule] = true;
     }
 
-    //nord-ouest
-    while (cellule - taille >= 0 && cellule % taille != 0 && matrice_adjacence[cellule][cellule - taille] && matrice_adjacence[cellule][cellule - 1 && matrice_adjacence[(cellule - taille) - 1][cellule - taille] && matrice_adjacence[(cellule - taille) - 1][cellule - 1]) {
-        cellule = (cellule - taille) - 1;
-        vue[cellule] = true
+    // sud-ouest
+    cellule = (entreX + entreY * taille);  // Reset cellule
+    while (cellule + taille < taille * taille && cellule % taille != 0 &&
+        matrice_adjacence[cellule][cellule + taille] &&
+        matrice_adjacence[cellule][cellule - 1] &&
+        matrice_adjacence[cellule + taille][cellule + taille - 1]) {
+        cellule = (cellule + taille) - 1;
+        vue[cellule] = true;
     }
+
+    // nord-ouest
+    cellule = (entreX + entreY * taille);  // Reset cellule
+    while (cellule - taille >= 0 && cellule % taille != 0 &&
+        matrice_adjacence[cellule][cellule - taille] &&
+        matrice_adjacence[cellule][cellule - 1] &&
+        matrice_adjacence[cellule - taille][cellule - taille - 1]) {
+        cellule = (cellule - taille) - 1;
+        vue[cellule] = true;
+    }
+
     return vue;
 }
