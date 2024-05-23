4a51ed948645eea919c859c0ee224991867505d2
léo <156224120+BUSCH-Leo@users.noreply.github.com>
2024-05-23 09:58:36 +0200

    visibilité affichage
    ajouté une fonction affichedonjonvisible pour un afficher le donjon comme avec fog of war

--- affichage/AfficheDonjon.cpp
+++ affichage/AfficheDonjon.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "../generation/generateur.cpp"
+#include "../deplacement/deplacement.cpp"
 
 using namespace std;
 
@@ -64,4 +65,111 @@ void AfficheDonjon(const vector<vector<bool>>& matrice_adjacence, int taille, in
         cout<<"██";
     }
     cout<<endl;
+}
+
+void AfficheDonjonVisible(const vector<vector<bool>>& matrice_adjacence, int taille, int sortieX, int sortieY, loot** LootsMatrix, int joueur_x, int joueur_y, bool**& visibleMatrix) {
+    vector<bool> vue = visibilite(matrice_adjacence, taille, joueur_x, joueur_y);
+
+    // Première ligne
+    cout << "  ";
+    for (int i = 0; i < taille * 2 + 1; ++i) {
+        cout << "██";
+    }
+    cout << endl;
+
+    // Lignes intermédiaires
+    for (int i = 0; i < taille; ++i) {
+        cout << "  ██";
+        for (int j = 0; j < taille; ++j) {
+            if (vue[i * taille + j]) {
+                if (LootsMatrix[i][j].type == "Joueur") cout << "\033[42m👤\033[0m";
+                else if ((i == 0 && j == 0) || (i == sortieX && j == sortieY)) cout << "\033[100m\033[96m[]\033[0m";
+                else if (LootsMatrix[i][j].type == "Ennemi") cout << "\033[100m\033[31mඞ!\033[0m";
+                else if (LootsMatrix[i][j].type == "Soin") cout << "\033[100m\033[92m♥" << LootsMatrix[i][j].value << "\033[0m";
+                else if (LootsMatrix[i][j].type == "Tresor") cout << "\033[100m\033[33m💰\033[0m";
+                else cout << "░░";
+                visibleMatrix[i][j] = true;
+                if (j != taille - 1) {
+                    if (matrice_adjacence[i * taille + j][i * taille + j + 1]) // Vérifie le noeud suivant
+                        cout << "  "; // Pas de mur
+                    else {
+                        cout << "██"; // Mur
+                    }
+                }
+            }
+            else if (visibleMatrix[i][j]) {
+                cout << "░░";
+                if (j != taille - 1) {
+                    if (matrice_adjacence[i * taille + j][i * taille + j + 1]) // Vérifie le noeud suivant
+                        cout << "  "; // Pas de mur
+                    else {
+                        cout << "██"; // Mur
+                    }
+                }
+            }
+            else {
+                cout << "▓▓";
+                if (j != taille - 1) {
+                    if (matrice_adjacence[i * taille + j][i * taille + j + 1]) // Vérifie le noeud suivant
+                        cout << "▓▓"; // Pas de mur
+                    else {
+                        cout << "▓▓"; // Mur
+                    }
+                }
+            }
+        }
+        cout << "██" << endl;
+
+        if (i != taille - 1) {
+            cout << "  ██";
+            for (int j = 0; j < taille; ++j) {
+                if (visibleMatrix[i][j]) {
+                    if (matrice_adjacence[i * taille + j][(i + 1) * taille + j]) // Vérifie le noeud en dessous
+                        cout << "  "; // Pas de mur
+                    else
+                        cout << "██"; // Mur
+                    if (j != taille - 1) {
+                        // Vérifier si les 4 nœuds adjacents sont reliés
+                        if (j < taille - 1 && i < taille - 1 &&
+                            matrice_adjacence[i * taille + j][i * taille + j + 1] && // Nœud suivant
+                            matrice_adjacence[i * taille + j][(i + 1) * taille + j] && // Nœud en dessous
+                            matrice_adjacence[i * taille + j + 1][(i + 1) * taille + j + 1] && // Nœud en diagonale en bas à droite
+                            matrice_adjacence[(i + 1) * taille + j][(i + 1) * taille + j + 1]) { // Nœud en diagonale en bas à gauche
+                            cout << "  "; // Pas de mur
+                        }
+                        else {
+                            cout << "██"; // Mur
+                        }
+                    }
+                }
+                else {
+                    if (matrice_adjacence[i * taille + j][(i + 1) * taille + j]) // Vérifie le noeud en dessous
+                        cout << "▓▓"; // Pas de mur
+                    else
+                        cout << "▓▓"; // Mur
+                    if (j != taille - 1) {
+                        // Vérifier si les 4 nœuds adjacents sont reliés
+                        if (j < taille - 1 && i < taille - 1 &&
+                            matrice_adjacence[i * taille + j][i * taille + j + 1] && // Nœud suivant
+                            matrice_adjacence[i * taille + j][(i + 1) * taille + j] && // Nœud en dessous
+                            matrice_adjacence[i * taille + j + 1][(i + 1) * taille + j + 1] && // Nœud en diagonale en bas à droite
+                            matrice_adjacence[(i + 1) * taille + j][(i + 1) * taille + j + 1]) { // Nœud en diagonale en bas à gauche
+                            cout << "▓▓"; // Pas de mur
+                        }
+                        else {
+                            cout << "▓▓"; // Mur
+                        }
+                    }
+                }
+            }
+            cout << "██" << endl;
+        }
+    }
+
+    // Dernière ligne
+    cout << "  ";
+    for (int i = 0; i < taille * 2 + 1; ++i) {
+        cout << "██";
+    }
+    cout << endl;
 }
