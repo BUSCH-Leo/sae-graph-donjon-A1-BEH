7c8414235de9cf734071bb987d8d5df83380c066
LeoHenriot <162290277+LeoHenriot@users.noreply.github.com>
2024-05-28 14:36:31 +0200

    Modification multiplicateur prix

--- jeu/shop.cpp
+++ jeu/shop.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int Shop(int joueur_coins, bool pass_paye, int nb_bonus, bool visibilite) {
+int Shop(int joueur_coins, bool pass_paye, float nb_bonus, bool visibilite) {
     int choix = 0;
 
     termkit::clear();
@@ -11,10 +11,10 @@ int Shop(int joueur_coins, bool pass_paye, int nb_bonus, bool visibilite) {
 
     cout << "\n     Voici les articles possible :" << endl;
     cout << "     - Pass PlusCourtChemin (" << (visibilite ? "20" : "40") << " 💰)     -> Choix 1" << endl;
-    cout << "     - InstaKill sur 1 ennemi (5 💰)    -> Choix 2" << endl;
+    cout << "     - InstaKill sur 1 ennemi (10 💰)   -> Choix 2" << endl;
     cout << "     - Potion de vie +5 ♥ (10 💰)       -> Choix 3" << endl;
     cout << "     - Téléportation aléatoire (5 💰)   -> Choix 4" << endl;
-    cout << "     - Double l'argent des sacs (" << 10*nb_bonus << " 💰) -> Choix 5" << endl;
+    cout << "     - Double l'argent des sacs (" << int(10*nb_bonus) << " 💰) -> Choix 5" << endl;
     cout << "     - Ressortir                        -> Choix 0" << endl;
 
     cout << "     Votre choix : ";
@@ -23,10 +23,10 @@ int Shop(int joueur_coins, bool pass_paye, int nb_bonus, bool visibilite) {
     while (choix < 0 || choix > 5
     || (choix == 1 && joueur_coins - 20 < 0 && visibilite == true)
     || (choix == 1 && joueur_coins - 40 < 0 && visibilite == false)
-    || (choix == 2 && joueur_coins - 5 < 0)
+    || (choix == 2 && joueur_coins - 10 < 0)
     || (choix == 3 && joueur_coins - 10 < 0)
     || (choix == 4 && joueur_coins - 5 < 0)
-    || (choix == 5 && ((joueur_coins - (10*nb_bonus)) < 0))
+    || (choix == 5 && ((joueur_coins - int(10*nb_bonus)) < 0))
     || (pass_paye == true && choix == 1)
     ) {
         if (pass_paye == true && choix == 1) {
